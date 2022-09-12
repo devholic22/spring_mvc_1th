9c5402324c7a66710e1a1d00fe04a972889f2377
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-09-13 01:41:07 +0900

    상품 등록 폼

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -46,4 +46,9 @@ public class BasicItemController {
         model.addAttribute("item", item);
         return "basic/item";
     }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
 }
